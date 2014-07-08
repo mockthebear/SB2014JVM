198cbd710599ce310a68b5dd652e075bdc924c17
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2014-07-08 19:26:54 -0300

    Adição de testes de lookupTable, tableSwitch, manipulação de objetos, array e matriz

--- projeto_JVM/T1.java
+++ projeto_JVM/T1.java
@@ -1,8 +1,16 @@
-class T1 {
-	private int i1;
+class T1 {private int i1;
 
 	public static void main(String[] args) {
-		doubleTest();
+            byteTest();
+            shortTest();
+            intTest();
+            floatTest();
+            doubleTest();
+            intArrayTest();
+            intTableTest();
+            lookupSwitchTest();
+            tableSwitchTest();
+            objectTest();
 	}
 	
 	static void intTest() {
@@ -81,4 +89,82 @@ class T1 {
 		System.out.println(a%b);
 		System.out.println(-a);
 	}
+    static void intArrayTest(){
+        int[] array = new int [300];
+        for(int i : array){
+            array[i] = i;
+            System.out.println(array[i]);
+        }
+    }
+    static void intTableTest(){
+        int[][] table = new int [100][100];
+        int i;
+        for(i=0;i<100;i++){
+            int j;
+            for(j=0;j<100;j++){
+                table[i][j] = i*100+j;
+                System.out.println(table[i][j]);
+            }
+        }
+    }
+    static void lookupSwitchTest(){
+        int i =0;
+        switch(i){
+            case 0:
+                
+                break;
+            case 1:
+                i=1;
+                break;
+            case 100000:
+                i=100000;
+                break;
+            case -100000:
+                i=-100000;
+                break;
+            case -234:
+                i=-234;
+                break;
+            case 39405:
+                i=39405;
+                break;
+            default:
+                i=-1;
+                break;
+        }
+        System.out.println(i);
+    }
+    static void tableSwitchTest(){
+        int i =0;
+        switch(i){
+            case 0:
+                
+                break;
+            case 1:
+                i=1;
+                break;
+            case 2:
+                i=2;
+                break;
+            case 3:
+                i=3;
+                break;
+            case 4:
+                i=4;
+                break;
+            case 5:
+                i=5;
+                break;
+            default:
+                i=-1;
+                break;
+        }
+        System.out.println(i);
+    }
+    static void objectTest(){
+        String obj = "primeiro objeto";
+        String obj2 = obj;
+        obj = "segundo objeto";
+        System.out.println("Mesmo objeto?"+obj==obj2+" e mesmo conteúdo"+obj.equals(obj2));
+    }
 }
