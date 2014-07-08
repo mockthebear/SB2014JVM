1cebf30825e9ad27e53eddb488c75035649a2643
getulioym <getulioym36@gmail.com>
2014-07-08 19:51:23 -0300

    Modificacao

--- projeto_JVM/t7.java
+++ projeto_JVM/t7.java
@@ -2,9 +2,16 @@ class t7 {
 
 	public static void main(String[] args) {
 		String a = new String("Hello!");
+		String b = new String("World!");
 		System.out.println(a);
+		System.out.println();
+		System.out.println(b);
+		Test(a, b);
 	}
-	static void Test(byte a, int b) {
+	static void Test(String a, String b) {
+		System.out.println(a);
+		System.out.println();
+		System.out.println(b);
 		System.out.println();
 	}
 
